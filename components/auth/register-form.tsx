0b08c3d434a34c29d603f7b4420d13c371c4292d
liren <327381633@qq.com>
2025-02-28 06:47:25 +0800

    feat: finish register form

---
+++ components/auth/register-form.tsx
@@ -0,0 +1,133 @@
+"use client";
+
+import { useTransition, useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { RegisterSchema } from "@/schemas/index";
+import { CardWrapper } from "@/components/auth/card-wrapper";
+import { Button } from "../ui/button";
+import { FormError } from "@/components/form/form-error";
+import { FormSuccess } from "@/components/form/form-success";
+import { register } from "@/actions/register";
+
+export function RegisterForm() {
+  const [error, setError] = useState<string | undefined>("");
+  const [success, setSuccess] = useState<string | undefined>("");
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
+  const handleSubmit = (values: z.infer<typeof RegisterSchema>) => {
+    // reset the error and suceess
+    setError("");
+    setSuccess("");
+
+    console.log("handle register, data: ", values);
+    startTransition(() => {
+      register(values).then((data) => {
+        setError(data.error);
+        setSuccess(data.success);
+      });
+    });
+  };
+
+  return (
+    <CardWrapper
+      headerLabel="Create an account"
+      backButtonLabel="Already have an account?"
+      backButtonHref="/auth/login"
+      showSocial
+    >
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(handleSubmit)}
+          // we could use space-y-4, but we want button to expand to the whole width, so flex is more suitable
+          className="space-y-6"
+        >
+          {/* the div here is just to have a narrow space between two inputs */}
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-lg">Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="John Doe"
+                      {...field}
+                      disabled={isPending}
+                      type="text"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-lg">Email</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="john@doe.com"
+                      {...field}
+                      disabled={isPending}
+                      type="email"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className="text-lg">Password</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="******"
+                      {...field}
+                      type="password"
+                      disabled={isPending}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          {/* 两个组件是否展示，依赖于 message 属性是否传值 */}
+          {/* TODO change them to disappear after some time */}
+          <FormError message={error} />
+          <FormSuccess message={success} />
+          <Button type="submit" className="w-full" disabled={isPending}>
+            Register
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
+}
--- schemas/index.ts
+++ schemas/index.ts
@@ -7,3 +7,18 @@ export const LoginSchema = z.object({
   // no need to validate the length of password, since some situations like first time login, the password won't satisfy the requirement
   password: z.string().nonempty("You must input your password!"),
 });
+
+export const RegisterSchema = z.object({
+  email: z.string().email({
+    message: "Email is required",
+  }),
+  password: z
+    .string()
+    .min(6, {
+      message: "Minimum 6 characters required",
+    })
+    .nonempty("You must input your password!"),
+  name: z.string().min(1, {
+    message: "Name is required",
+  }),
+});
